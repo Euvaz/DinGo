e21891ca9cafa4c007111f7ea7ec1d96263e52d7
euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2022-01-18 15:16:07 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gost
+
+Gost is an in-progress bot-rewrite of Squiddie, which will be written in Go instead of Python due to the discontinutation of `discord.py`.
+
+## LICENSE
+
+As of 18-Jan-2022, Gost is fully open to the public, licensed under GPLv2.
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	//"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+// Variables used for command line parameters
+var (
+	token string
+)
+
+func init() {
+    //flag.StringVar(&token, "t", "", "Bot Token")
+    //flag.Parse()
+
+    // Load dotenv into environment
+    godotenv.Load() 
+}
+
+func main() {
+    // Load discord token
+    token := os.Getenv("DISCORD_TOKEN")
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+    
+    // Define bot prefix
+    PREFIX := "!sd"
+    
+    // Check for command prefix
+    if strings.HasPrefix(m.Content, PREFIX) {
+        s.ChannelMessageSend(m.ChannelID, "Working")
+    }
+}
